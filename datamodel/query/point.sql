3961b911dbeb0467e3d1ad313ee3cd40410c7603
Makin9 <n75seven@gmail.com>
2023-12-13 21:47:26 +0900

    pointを修正

--- datamodel/query/point.sql
+++ datamodel/query/point.sql
@@ -3,21 +3,14 @@ CREATE TABLE point_cards (
     user_id INT REFERENCES users(user_id),
     total_points INT DEFAULT 0,
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    points_expire_date TIMESTAMP
 );
 
 CREATE TABLE point_history (
     history_id SERIAL PRIMARY KEY,
     card_id INT REFERENCES point_cards(card_id),
     point_change INT NOT NULL,
+    change_type TEXT CHECK (change_type IN ('use', 'save')),
     change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    transaction_id INT
-);
-
-CREATE TABLE transactions (
-    transaction_id SERIAL PRIMARY KEY,
     order_id INT
-    order_type TEXT CHECK (my_column IN ('ec_order', 'mobile_order'))
-    amount DECIMAL(10, 2),
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    order_type TEXT CHECK (order_type IN ('ec_order', 'mobile_order'))
 );
--- prompt/point.md
+++ prompt/point.md
@@ -54,16 +54,16 @@ CREATE TABLE point_cards (
     user_id INT REFERENCES users(user_id),
     total_points INT DEFAULT 0,
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    points_expire_date TIMESTAMP
 );
 
 CREATE TABLE point_history (
     history_id SERIAL PRIMARY KEY,
     card_id INT REFERENCES point_cards(card_id),
     point_change INT NOT NULL,
+    change_type TEXT CHECK (change_type IN ('use', 'save')),
     change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     order_id INT
-    order_type TEXT CHECK (my_column IN ('ec_order', 'mobile_order'))
+    order_type TEXT CHECK (order_type IN ('ec_order', 'mobile_order'))
 );
 
 
